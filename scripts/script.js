d2c304a75cd856546aeb40c3b6eb4b221574dce7
ColinTwi <145386329+ColinTwi@users.noreply.github.com>
2024-09-19 22:36:30 +0200

    files toegevoegd week 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
-console.log("hi");
+console.log("test");
+
+var shareKnop = document.querySelector(".profielKnoppen button:nth-of-type(1)");
+var editKnop = document.querySelector(".profielKnoppen button:nth-of-type(2)");
+
+
+shareKnop.onclick = veranderKnop1; 
+editKnop.onclick = veranderKnop2;
+
+function veranderKnop1() {
+    shareKnop.classList.toggle("geklikt")
+}
+
+function veranderKnop2() {
+    editKnop.classList.toggle("geklikt")
+}
